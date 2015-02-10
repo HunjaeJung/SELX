8ad2487abf88fff1e4022a5a1319657dd7e8adf1
Fermata <fermata@Fermatas-MacBook-Pro.local>
2015-02-10 13:45:07 -0500

    added battery gauge controller

--- Web/static/js/gaugecontroller.js
+++ Web/static/js/gaugecontroller.js
@@ -11,5 +11,13 @@ var gauge = {
 		var point = chart.series[0].points[0];
 		chart.yAxis[0].setExtremes(0,max);
 		point.update(value);
+	},
+	setBatteryGauge : function (max, value) {
+		var percentage = value / max * 100;
+		percentage = percentage.toFixed(1) + "%";
+		$("#bat-percent").html(percentage);
+		$("#bat-current").html(value);
+		$("#bat-capacity").html(max);
+		$("#battery-bar").css({width:percentage});
 	}
 }
